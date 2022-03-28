6f3a9e9325adb63c40a7774f9dca8e98baedfcbb
HAPPY <HAPPY@LAPTOP-LIP92R15>
2022-03-28 12:54:30 +0530

    first commit

---
+++ happy123/src/test/java/happy123/CreatecontactTest.java
@@ -0,0 +1,17 @@
+package happy123;
+
+import org.testng.annotations.Test;
+
+public class CreatecontactTest {
+	@Test(groups="smoke")
+	public void Createcontact() {
+		System.out.println("contact created");
+		
+		
+		System.out.println("dibya");
+		
+		
+		//fbjafsbhj
+	}
+
+}
---
+++ happy123/src/test/java/happy123/DeleteContactTest.java
@@ -0,0 +1,11 @@
+package happy123;
+
+import org.testng.annotations.Test;
+
+public class DeleteContactTest {
+	@Test(groups="regression")
+	public void deleteContact() {
+		System.out.println("delete contact");
+	}
+
+}
